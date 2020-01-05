6e14a925930781c3bf7b2834b1b22540dfcc9d56
Qihao He <qi.he@maine.edu>
2020-01-05 18:07:55 -0500

    practice activity #5 dependency injdection with annotations, read lists from file, load list to array, when getFortune() is called, return a random fortune from array, inject new dependency into your coach implementation.

--- src/com/luv2code/springdemo/ReadFileFortuneService.java
+++ src/com/luv2code/springdemo/ReadFileFortuneService.java
@@ -1,17 +1,42 @@
 package com.luv2code.springdemo;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReadFileFortuneService implements FortuneService {
 	
-	@Value("${foo.fortune}")
-	private String fortune;
+	private String fileName = "/Users/qihaohe/eclipse-workspace/spring-demo-annotations/src/fortune-data.txt";
+	private List<String> fortuneList;
+
+	public ReadFileFortuneService() {
+		File file = new File(fileName);
+		System.out.println("Reading fortunes from file: " + file);
+		System.out.println("Fiel exists: " + file.exists());
+		fortuneList = new ArrayList<>();
+		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String tempLine;
+			while((tempLine = br.readLine()) != null) {
+				fortuneList.add(tempLine); 
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public String getFortune() {
-		return fortune;
+		int i = random.nextInt(fortuneList.size());
+		String tempFortune = fortuneList.get(i);
+		return tempFortune;
 	}
-
+	
+	private Random random = new Random();
 }
